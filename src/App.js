a638f9449e653515111a1d08279a5441261d075c
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-11-06 10:35:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import Signin from './components/signin';
 import About from './components/About';
 import Contact from './components/contacts';
 
-function App() {
+function App ()
+  { 
+  
   return (
     <BrowserRouter>
     <Navbar/>
