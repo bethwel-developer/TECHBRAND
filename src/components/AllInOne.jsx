6df6b7e4dedcea24cb8f56f2493f3ae5f52834cb
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-12-11 22:07:29 +0300

    Update AllInOne.jsx

--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -5,7 +5,7 @@ import { CheckIcon } from '@heroicons/react/outline';
 
 const AllInOne = () => {
   return (
-    
+
     <div name='platforms' className='w-full my-32'>
       <div className='max-w-[1240px] mx-auto px-2'>
         <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
